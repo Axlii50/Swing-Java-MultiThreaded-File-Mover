dd6594152460b55b79f80da3d5ea8729240fe6b3
AcePeQ <maciej.nojszewski22@gmail.com>
2024-05-14 20:56:16 +0200

    Fix buttonListeners and set Source and Destination to absolute filePath on click

--- src/main/java/pl/wit/Main.java
+++ src/main/java/pl/wit/Main.java
@@ -10,17 +10,18 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public class Main {
-    private String Source = null;
-    private String Destination = null;
+    private static String Source = null;
+    private static String Destination = null;
     static MainWindow window;
     public static void main(String[] args) {
 
         window = new MainWindow();
 
-        window.setStartButtonActionListener(StartButtonListener());
+        window.setSourceButtonActionListener(SourceButtonListener());
+        window.setDestinationButtonActionListener(DestinationButtonListener());
     }
 
-    private static ActionListener StartButtonListener() {
+    private static ActionListener SourceButtonListener() {
         return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -32,6 +33,27 @@ public class Main {
 
                 if (result == JFileChooser.APPROVE_OPTION) {
                     File selectedFile = chooser.getSelectedFile();
+                    window.getSourcePathLabelComponent().getLabel().setText(selectedFile.getAbsolutePath());
+                    Source = selectedFile.getAbsolutePath();
+                }
+            }
+        };
+    }
+
+    private static ActionListener DestinationButtonListener() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setDialogTitle("Wybierz folder");
+                chooser.setFileSystemView(FileSystemView.getFileSystemView());
+
+                int result = chooser.showOpenDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = chooser.getSelectedFile();
+                    window.getDestinationPathLabelComponent().getLabel().setText(selectedFile.getAbsolutePath());
+                    Destination = selectedFile.getAbsolutePath();
                 }
             }
         };
