b46373e09e712bbf5b6b90ae9a60812ae53286d5
Axlii50 <jakubstegienko@gmail.com>
2024-05-14 20:30:54 +0200

    connecting backend with frontend part1

--- src/main/java/pl/wit/Main.java
+++ src/main/java/pl/wit/Main.java
@@ -1,17 +1,39 @@
 package pl.wit;
 
+import pl.wit.Components.LabelComponent;
 import pl.wit.Windows.MainWindow;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 public class Main {
+    private String Source = null;
+    private String Destination = null;
     static MainWindow window;
     public static void main(String[] args) {
 
         window = new MainWindow();
 
+        window.setStartButtonActionListener(StartButtonListener());
+    }
 
+    private static ActionListener StartButtonListener() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setDialogTitle("Wybierz folder");
+                chooser.setFileSystemView(FileSystemView.getFileSystemView());
 
+                int result = chooser.showOpenDialog(null);
 
-
-
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = chooser.getSelectedFile();
+                }
+            }
+        };
     }
 }
