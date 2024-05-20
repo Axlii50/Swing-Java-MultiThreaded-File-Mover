1ac2a204817c51962460122c9c1bad5c5677c194
Axlii50 <jakubstegienko@gmail.com>
2024-05-20 21:50:18 +0200

    bugfix

--- src/main/java/pl/wit/Main.java
+++ src/main/java/pl/wit/Main.java
@@ -19,7 +19,6 @@ public class Main {
     public static void main(String[] args) {
         threadService = new ThreadService(Runtime.getRuntime().availableProcessors());
         window = new MainWindow();
-
         window.setStartButtonActionListener(StartButtonListener());
     }
 
