29d2b3bfb69a113a8c50f4c7aa1830246cc1625d
Adamss5 <adam.bartos002@gmail.com>
2024-05-21 13:49:42 +0200

    dodawanie komentarzy v1.4856

---
+++ src/main/java/pl/wit/Components/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * This package contains utility classes for processing data.
+ * <p>
+ * The classes in this package provide various utility methods for
+ * manipulating and analyzing data structures such as lists, sets,
+ * and maps.
+ * </p>
+ *
+ * <h2>Key Classes</h2>
+ * <ul>
+ * <li>{@link pl.wit.Components.ButtonComponent} - package komponentów.</li>
+ * </ul>
+ *
+ * @since 1.0
+ * @version 1.2
+ */
+package pl.wit.Components;
+
