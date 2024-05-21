9c502796f27c10b1e3c7ec79658c875d3930c69f
Adamss5 <adam.bartos002@gmail.com>
2024-05-21 12:20:16 +0200

    dodawanie komentarzy v1.4856

--- src/main/java/pl/wit/FileService.java
+++ src/main/java/pl/wit/FileService.java
@@ -8,12 +8,31 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Klasa odpowiedzialna za kopiowanie plików i katalogów
+ */
 public class FileService implements Runnable {
 
+    /**
+     * Struktura plików do skopiowania
+     */
     private final Node fileStructure;
+    /**
+     * Ścieżka docelowa do kopiowania plików
+     */
     private final String destination;
+    /**
+     * Serwis wykonawczy do zarządzania wątkami
+     */
     private final ExecutorService executor;
 
+    /**
+     * Konstruktor tworzący serwis plików
+     *
+     * @param fileStructure struktura plików do skopiowania
+     * @param destination ścieżka docelowa do kopiowania plików
+     * @param executor serwis wykonawczy do zarządzania wątkami
+     */
     public FileService(Node fileStructure, String destination, ExecutorService executor){
 
         this.fileStructure = fileStructure;
@@ -22,46 +41,92 @@ public class FileService implements Runnable {
 
     }
 
+    /**
+     * Metoda uruchamiana w nowym wątku, rozpoczynająca proces kopiowania
+     */
     @Override
     public void run() {
-        AtomicInteger activeTasks = new AtomicInteger(1);  // Start with 1 to account for the initial task
+
+        /**
+         * Licznik aktywnych zadań
+         */
+        AtomicInteger activeTasks = new AtomicInteger(1);
+
+        /**
+         * Synchronizator pozwalający na czekanie na zakończenie wszystkich zadań
+         */
         CountDownLatch latch = new CountDownLatch(1);
 
+        /**
+         * Rozpoczęcie procesu kopiowania katalogu, przekazując strukturę plików, ścieżkę docelową, licznik aktywnych zadań oraz synchronizator
+         */
         copyDirectory(fileStructure, destination, activeTasks, latch);
 
         try {
-            latch.await();  // Wait for all tasks to complete
+            /**
+             * Czekanie na zakończenie wszystkich zadań
+             */
+            latch.await();
         } catch (InterruptedException e) {
+            /**
+             * Obsługiwanie przerwania oczekiwania na zakończenie zadań
+             */
             throw new RuntimeException(e);
         }
+        /**
+         * Zamykanie wykonawcy, aby nie przyjmował nowych zadań
+         */
         executor.shutdown();
         try {
+            /**
+             * Oczekiwanie na zakończenie wszystkich aktywnych zadań wykonywanych przez wykonawcę
+             */
             executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
+    /**
+     * Obsługiwanie przerwania oczekiwania
+     */
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Kopiowanie katalogu i jego zawartości rekurencyjnie
+     *
+     * @param node węzeł katalogu do skopiowania
+     * @param destinationPath ścieżka docelowa do skopiowania katalogu
+     * @param activeTasks licznik aktywnych zadań
+     * @param latch synchronizator
+     */
     private void copyDirectory(Node node, String destinationPath, AtomicInteger activeTasks, CountDownLatch latch) {
         createDirectory(destinationPath);
 
-        //iterujemy po kazdym dziecku rodzica
+        /**
+         * Iteracja po każdym dziecku rodzica
+         */
         for (Node child : node.getChildren()) {
-            //tworzymy sciezke dla nowego elementu bazujac na jego nazwie
+
+            /**
+             * Utworzenie ścieżki dla nowego elementu bazując na jego nazwie
+             */
             String newDestinationPath = destinationPath + "\\" + child.getName();
-            //zwiekszamy ilosc akwywnych wątków
+
+            /**
+             * Zwiększenie liczby aktywnych zadań
+             */
             activeTasks.incrementAndGet();
-            //startujemy nowe zadanie do wykonania
+
+            /**
+             * Startowanie nowego zadania do wykonania
+             */
             executor.execute(() -> {
                 try {
-                    //wykonaj adekwatne do typu zadanie
                     if (child instanceof Leaf && child.getChildren().isEmpty()) {
                         copyFile(child.getPath(), newDestinationPath);
                     } else {
                         copyDirectory(child, newDestinationPath, activeTasks, latch);
                     }
                 } finally {
-                    //jeżeli wszystkie zadania zostały zakonczone to zwalniamy blokade (latch)
                     if (activeTasks.decrementAndGet() == 0) {
                         latch.countDown();
                     }
@@ -69,7 +134,6 @@ public class FileService implements Runnable {
             });
         }
 
-        //jeżeli wszystkie zadania zostały zakonczone to zwalniamy blokade (latch)
         if (activeTasks.decrementAndGet() == 0) {
             latch.countDown();
         }
